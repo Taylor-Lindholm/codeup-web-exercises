ce326db686247040b3b7d16add0015a10c616051
taylor lindholm <taylor.lindholm01@gmail.com>
2021-04-10 20:07:09 -0500

    feat: added jquery click events.

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,11 +1,29 @@
 
 window.onload = function () {
-    alert(' Five thousand years later, the page has loaded.');
+    console.log(' Five thousand years later, the page has loaded.');
 }
 
-$('*').css('background-color', '#ff0')
+// jquery selectors exercise
+// $('*').css('background-color', '#ff0')
+//
+// $('h3, p').css('font-size', '5em');
+//
+// var contents = $('h4').html();
+// alert(contents);
 
-$('h3, p').css('font-size', '5em');
+//jquery mouse events exercise
 
-var contents = $('h4').html();
-alert(contents);
+$('h1').click(function (){
+    $(this).css('background-color','#46ff00');
+});
+
+$('p').dblclick(function(){
+    $(this).css('font-size', 18);
+})
+
+$('li').hover(
+    function (){
+        $(this).css('font-color', '#ac005e');
+    }, function (){
+        $(this).css('font-color', '#000');
+})
